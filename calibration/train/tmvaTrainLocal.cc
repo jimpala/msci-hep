1c6d67ae350871de1e33f64fd0bc6c0275051db1
James Pyne <james.pyne.13@ucl.ac.uk>
2016-10-10 11:22:46 +0100

    Latest training scripts and root files downloaded.

--- calibration/train/tmvaTrainLocal.cc
+++ calibration/train/tmvaTrainLocal.cc
@@ -15,6 +15,8 @@ int main(int argc, char * argv[]) {
 
   bool lowpt = ( std::atoi(argv[2]) == 1 );
 
+//  bool tracks = false;
+  bool tracks = false;
   bool useMET = false;
 
   std::cout << "Low pt " << lowpt << std::endl;
@@ -26,9 +28,14 @@ int main(int argc, char * argv[]) {
     TString output_file_pt = (lowpt) ? "_lowpt" : "";
     TString output_file_met = (useMET) ? "_MET" : "";
     TString kFold = (i==0) ? "_even" : "_odd";
+    TString trackstr = (tracks) ? "_tracks" : "";
 
-    TFile* outputFile = TFile::Open( "TMVA"+output_file+output_file_pt+output_file_met+kFold+".root", "RECREATE" );
-    TMVA::Factory* factory = new TMVA::Factory("tmvaTest"+output_file+output_file_pt+output_file_met+kFold, outputFile, "");
+    TFile* outputFile = TFile::Open( "TMVA"+output_file+output_file_pt+output_file_met+kFold+trackstr+".root", "RECREATE" );
+    TMVA::Factory* factory = new TMVA::Factory("tmvaTest"+output_file+output_file_pt+output_file_met+trackstr+kFold, outputFile, "");
+//  TFile* trainingFile = new TFile("./../../../PGS/outputGeneral_fit_ntuple.mc15_13TeV.410000.DAOD_FTAG2_rescaling_train.root");
+//  TFile* testFile = new TFile("./../../../PGS/outputGeneral_fit_ntuple.mc15_13TeV.410000.DAOD_FTAG2_rescaling_test.root");
+//  TFile* trainingFile = new TFile("./../../../PGS/outputGeneral_fit_all_rescaling_train.root");
+//  TFile* testFile = new TFile("./../../../PGS/outputGeneral_fit_all_rescaling_test.root");
 
 
     TString signal = "tree_55";
@@ -45,19 +52,23 @@ int main(int argc, char * argv[]) {
 
     std::vector<std::string> signalFiles;
     signalFiles.push_back("");
+    /*
     signalFiles.push_back("SysMCAtNLO");
     signalFiles.push_back("SysPowhegHerwig");
     signalFiles.push_back("SysttbarRadHi");
     signalFiles.push_back("SysttbarRadLo");
-
+    */
     for(int r=0;r<signalFiles.size();r++){
       int entriesToGet = 30000;
-      sig->Add(("../PGS/outputGeneral_fit_top"+signalFiles.at(r)+"_rescaling.root").c_str());
-//      sig->Add(("/unix/atlasvhbb/abell/mc15data7/PGS/outputGeneral_fit_top"+signalFiles.at(r)+"_rescaling.root").c_str());
+      if(tracks) sig->Add(("/unix/atlas3/abell/tmvaTraining/PGS_tracks/outputGeneral_fit_top"+signalFiles.at(r)+"_rescaling.root").c_str());
+      else sig->Add(("../PGS/outputGeneral_fit_top"+signalFiles.at(r)+"_rescaling.root").c_str());
+//      sig->Add(("/unix/atlasvhbb/abell/mc15data7/PGS/outputGeneral_fit_top"+signalFiles.at(r)+"_rescaling.root").c_str(), entriesToGet);
+//      std::cout << sig->GetEntries() <<std::endl;
     }
 //    TFile* inputFileSignal = new TFile("/unix/atlasvhbb/abell/mc15data7/PGS/outputGeneral_fit_top_all_rescaling.root");
-    TFile* inputFile = new TFile("../PGS/outputGeneral_fit_all_rescaling.root");
-//    TFile* inputFile = new TFile("/unix/atlasvhbb/abell/mc15data7/PGS/outputGeneral_fit_all_rescaling.root");
+    TFile* inputFile; // = new TFile("/unix/atlasvhbb/abell/mc15data7/PGS/outputGeneral_fit_all_rescaling.root");
+    if(tracks) inputFile = TFile::Open("/unix/atlas3/abell/tmvaTraining/PGS_tracks/outputGeneral_fit_all_rescaling.root");
+    else inputFile = TFile::Open("../PGS/outputGeneral_fit_all_rescaling.root");
 
 // get the TTree objects from the input files
 
@@ -106,39 +117,36 @@ int main(int argc, char * argv[]) {
 //  factory->AddVariable("train_met", 'F');
 
     TCut mycut = "";
+    TCut lowpT = (tracks) ? "0.02" : "0.02";
 
-    if(lowpt){
-      mycut = "sub_leading_pt < 0.03  && (leptontype==0 || (etmiss>60e3 && (mll<80e3 || mll>100e3) && mll>50e3))";
-      if(jetnum==3) mycut = "sub_leading_pt < 0.03 || sub_sub_leading_pt < 0.03 && (leptontype==0 || (etmiss>60e3 && (mll<80e3 || mll>100e3) && mll>50e3))";
+    if(tracks){
+      if(lowpt){
+	mycut = "sub_leading_pt < 0.02  && (leptontype==0 || (etmiss>60e3 && (mll<80e3 || mll>100e3) && mll>50e3))";
+	if(jetnum==3) mycut = "sub_leading_pt < 0.02 || sub_sub_leading_pt < 0.02 && (leptontype==0 || (etmiss>60e3 && (mll<80e3 || mll>100e3) && mll>50e3))";
+      }
+      else {
+	mycut = "sub_leading_pt > 0.02 && (leptontype==0 || (etmiss>60e3 && (mll<80e3 || mll>100e3) && mll>50e3))";
+	if(jetnum==3) mycut = "sub_leading_pt > 0.02 && sub_sub_leading_pt > 0.02  && (leptontype==0 || (etmiss>60e3 && (mll<80e3 || mll>100e3) && mll>50e3))";
+      }
+    }else{
+      if(lowpt){
+	mycut = "sub_leading_pt < 0.03  && (leptontype==0 || (etmiss>60e3 && (mll<80e3 || mll>100e3) && mll>50e3))";
+	if(jetnum==3) mycut = "sub_leading_pt < 0.03 || sub_sub_leading_pt < 0.03 && (leptontype==0 || (etmiss>60e3 && (mll<80e3 || mll>100e3) && mll>50e3))";
+      }
+      else {
+	mycut = "sub_leading_pt > 0.03 && (leptontype==0 || (etmiss>60e3 && (mll<80e3 || mll>100e3) && mll>50e3))";
+	if(jetnum==3) mycut = "sub_leading_pt > 0.03 && sub_sub_leading_pt > 0.03  && (leptontype==0 || (etmiss>60e3 && (mll<80e3 || mll>100e3) && mll>50e3))";
+      }
     }
-    else {
-      mycut = "sub_leading_pt > 0.03 && (leptontype==0 || (etmiss>60e3 && (mll<80e3 || mll>100e3) && mll>50e3))";
-      if(jetnum==3) mycut = "sub_leading_pt > 0.03 && sub_sub_leading_pt > 0.03  && (leptontype==0 || (etmiss>60e3 && (mll<80e3 || mll>100e3) && mll>50e3))";
-    }
+
   
 // Tell the factory how to use the training and testing events
     factory->PrepareTrainingAndTestTree( mycut, "NormMode=None:!V" );
-//    factory->PrepareTrainingAndTestTree( mycut && mycut2, "SplitMode=random:!V" );
 
-// Book MVA methods (see TMVA manual).  
+// Book MVA methods (see TMVA manual).    
 
-//  factory->BookMethod(TMVA::Types::kFisher, "Fisher", "H:!V:Fisher");  
-//  factory->BookMethod(TMVA::Types::kBDT, "BDT", "NTrees=800");
-//  factory->BookMethod(TMVA::Types::kBDT, "BDT_Advanced","IgnoreNegWeightsInTraining:!H:!V:NTrees=800:MaxDepth=4:BoostType=AdaBoost:AdaBoostBeta=0.15:SeparationType=GiniIndex:nCuts=100:PruneMethod=NoPruning");
-//    factory->BookMethod(TMVA::Types::kBDT, "BDT_Overtrain_reference","IgnoreNegWeightsInTraining:!H:!V:NTrees=200:MaxDepth=4:BoostType=AdaBoost:AdaBoostBeta=0.15:SeparationType=GiniIndex:nCuts=100:PruneMethod=NoPruning");
-
-    factory->BookMethod(TMVA::Types::kBDT, "BDT_Overtrain_reference","IgnoreNegWeightsInTraining:!H:!V:NTrees=200:MaxDepth=4:BoostType=Grad:Shrinkage=0.10:SeparationType=GiniIndex:nCuts=100:PruneMethod=NoPruning");
-
-//    factory->BookMethod( TMVA::Types::kBDT, "BDTG","!H:!V:NTrees=1000:MinNodeSize=2.5%:BoostType=Grad:Shrinkage=0.10:UseBaggedBoost:BaggedSampleFraction=0.5:nCuts=20:MaxDepth=2" );
-
-//  factory->BookMethod(TMVA::Types::kBDT, "BDT_Overtrain_safe","IgnoreNegWeightsInTraining:!H:!V:NTrees=200:MaxDepth=4:BoostType=AdaBoost:AdaBoostBeta=0.15:SeparationType=GiniIndex:nCuts=100:PruneMethod=NoPruning");
-//  factory->BookMethod(TMVA::Types::kBDT, "BDT_Overtrain_mega_safe","IgnoreNegWeightsInTraining:!H:!V:NTrees=100:MaxDepth=4:BoostType=AdaBoost:AdaBoostBeta=0.15:SeparationType=GiniIndex:nCuts=100:PruneMethod=NoPruning");
-
-
-//  factory->BookMethod(TMVA::Types::kBDT, "BDT_Advanced_NoIgnore","!H:!V:NTrees=800:MaxDepth=4:BoostType=AdaBoost:AdaBoostBeta=0.15:SeparationType=GiniIndex:nCuts=100:PruneMethod=NoPruning");
-
-//  factory->BookMethod(TMVA::Types::kBDT, "BDT_Advanced_Deep","!H:!V:NTrees=800:MaxDepth=100:BoostType=AdaBoost:AdaBoostBeta=0.15:SeparationType=GiniIndex:nCuts=100:PruneMethod=NoPruning");
-//  factory->BookMethod(TMVA::Types::kBDT, "BDT_Advanced_original","!H:!V:NTrees=200:MaxDepth=4:BoostType=AdaBoost:AdaBoostBeta=0.15:SeparationType=GiniIndex:nCuts=100:NEventsMin=0.1:PruneMethod=NoPruning");
+    factory->BookMethod(TMVA::Types::kBDT, "BDT_ABbase","IgnoreNegWeightsInTraining:!H:!V:NTrees=200:MaxDepth=4:BoostType=AdaBoost:AdaBoostBeta=0.10:SeparationType=GiniIndex:nCuts=100:PruneMethod=NoPruning");
+    factory->BookMethod(TMVA::Types::kBDT, "BDT_GBMbase","IgnoreNegWeightsInTraining:!H:!V:NTrees=200:MaxDepth=4:BoostType=Grad:Shrinkage=0.10:SeparationType=GiniIndex:nCuts=100:PruneMethod=NoPruning");
 
 // Train, test and evaluate all methods
 
