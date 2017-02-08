c811549c2df581b4ba0a16d192b5b6c63a0a1f2f
James Pyne <james.pyne.13@ucl.ac.uk>
2017-02-08 15:14:28 +0000

    Fixing code to map event numbers to sample types.

--- skl_analysis/BDT_analysis.py
+++ skl_analysis/BDT_analysis.py
@@ -1,3 +1,4 @@
+import itertools
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,29 +9,42 @@ from sklearn.model_selection import train_test_split
 
 
 sample_map = {
-    'qqZvvH125': 1,
-    'qqWlvH125': 2,
-    'Wbb': 3,
-    'Wbc': 4,
-    'Wcc': 5,
-    'Wbl': 6,
-    'Wcl': 7,
-    'Wl': 8,
-    'Zbb': 9,
-    'Zbc': 10,
-    'Zcc': 11,
-    'Zbl': 12,
-    'Zcl': 13,
-    'Zl': 14,
-    'ttbar': 15,
-    'stopt': 16,
-    'stops': 17,
-    'stopWt': 18,
-    'WW': 19,
-    'ZZ': 20,
-    'WZ': 21
+    'qqZvvH125': 'VH',
+    'qqWlvH125': 'VH',
+    'Wbb': 'V+jets',
+    'Wbc': 'V+jets',
+    'Wcc': 'V+jets',
+    'Wbl': 'V+jets',
+    'Wcl': 'V+jets',
+    'Wl': 'V+jets',
+    'Zbb': 'V+jets',
+    'Zbc': 'V+jets',
+    'Zcc': 'V+jets',
+    'Zbl': 'V+jets',
+    'Zcl': 'V+jets',
+    'Zl': 'V+jets',
+    'ttbar': 'ttbar',
+    'stopt': 'stop',
+    'stops': 'stop',
+    'stopWt': 'stop',
+    'WW': 'diboson',
+    'ZZ': 'diboson',
+    'WZ': 'diboson'
 }
 
+reduced_sample_map = dict()
+
+signal_zipped = dict(zip(range(1, 4), ['VH' for x in range(3)]))
+Vjets_zipped = dict(zip(range(4, 15), ['V+jets' for x in range(11)]))
+ttbar_zipped = {15: 'ttbar'}
+stop_zipped = dict(zip(range(16, 19), ['stop' for x in range(3)]))
+diboson_zipped = dict(zip(range(19, 22), ['diboson' for x in range(3)]))
+
+reduced_sample_map.update(signal_zipped)
+reduced_sample_map.update(Vjets_zipped)
+reduced_sample_map.update(ttbar_zipped)
+reduced_sample_map.update(stop_zipped)
+reduced_sample_map.update(diboson_zipped)
 
 branch_names = ["sample", "EventWeight", "EventNumber", "nJ", "nBJ", "mBB",
                 "dRBB", "dPhiBB", "dEtaBB",
@@ -39,7 +53,7 @@ branch_names = ["sample", "EventWeight", "EventNumber", "nJ", "nBJ", "mBB",
                 "mTW", "MET", "pTJ3", "mBBJ", "BDT"]
 
 # Read in NTuple data from pre-processed CSV file.
-df = pd.read_csv('/Volumes/HEPDRIVE/VHbb-data/CSV/VHbb_data_preselect.csv')
+df = pd.read_csv('/Volumes/HEPDRIVE/VHbb-data/CSV/VHbb_data_preselect.csv', index_col=0)
 
 # Split into 2 jet and 3 jet trainings.
 df_2jet = df[df['nJ'] == 2]
@@ -72,13 +86,31 @@ train_3jet, test_3jet, train_3jet_class, test_3jet_class = train_test_split(data
 
 # Preserve event numbers of test sets, then drop from training and test sets.
 
-test_2jet_event_nos = test_2jet[:, 0]
-test_3jet_event_nos = test_3jet[:, 0]
+test_2jet_event_nos = test_2jet[:, 0].astype(int)
+test_3jet_event_nos = test_3jet[:, 0].astype(int)
 
 train_2jet = np.delete(train_2jet, 0, 1)
 train_3jet = np.delete(train_3jet, 0, 1)
 test_2jet = np.delete(test_2jet, 0, 1)
 test_3jet = np.delete(test_3jet, 0, 1)
+
+# Map event numbers to process type numbers.
+test_2jet_process_no = map(lambda a: df[df['EventNumber'] == a].reset_index(drop=True).get_value(0,'sample'),
+                           test_2jet_event_nos)
+test_3jet_process_no = map(lambda a: df[df['EventNumber'] == a].reset_index(drop=True).get_value(0,'sample'),
+                           test_3jet_event_nos)
+
+# Map process types numbers to process type groups.
+# Note that this is a convoluted process and could be
+# simplified at a later date...
+test_2jet_processes = map(lambda a: reduced_sample_map[a], test_2jet_process_no)
+test_3jet_processes = map(lambda a: reduced_sample_map[a], test_3jet_process_no)
+
+
+
+#################
+#     2 JET     #
+#################
 
 # Create BDT object.
 bdt = AdaBoostClassifier(DecisionTreeClassifier(max_depth=3),
@@ -88,8 +120,10 @@ bdt = AdaBoostClassifier(DecisionTreeClassifier(max_depth=3),
 # Train BDT for 2 jet.
 bdt.fit(train_2jet, train_2jet_class)
 
-# Plot decision scores for test set.
+# Get decision scores for test set.
 twoclass_output = bdt.decision_function(test_2jet)
+
+# Plot decision histogram.
 plot_range = (twoclass_output.min(), twoclass_output.max())
 plt.subplot(122)
 
