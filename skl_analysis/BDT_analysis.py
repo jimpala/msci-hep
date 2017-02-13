bae4ed7bb5177d73f7f6583f35bffea670caf339
James Pyne <james.pyne.13@ucl.ac.uk>
2017-02-13 21:02:17 +0000

    Now plotting individual backgrounds rather than subsets.

--- skl_analysis/BDT_analysis.py
+++ skl_analysis/BDT_analysis.py
@@ -6,8 +6,33 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 
-
 sample_map = {
+    'qqZvvH125': 1,
+    'qqWlvH125': 2,
+    'Wbb': 3,
+    'Wbc': 4,
+    'Wcc': 5,
+    'Wbl': 6,
+    'Wcl': 7,
+    'Wl': 8,
+    'Zbb': 9,
+    'Zbc': 10,
+    'Zcc': 11,
+    'Zbl': 12,
+    'Zcl': 13,
+    'Zl': 14,
+    'ttbar': 15,
+    'stopt': 16,
+    'stops': 17,
+    'stopWt': 18,
+    'WW': 19,
+    'ZZ': 20,
+    'WZ': 21
+}
+sample_map_invert = dict(zip(sample_map.values(),sample_map.keys()))
+
+
+reduce_map = {
     'qqZvvH125': 'VH',
     'qqWlvH125': 'VH',
     'Wbb': 'V+jets',
@@ -33,8 +58,8 @@ sample_map = {
 
 reduced_sample_map = dict()
 
-signal_zipped = dict(zip(range(1, 4), ['VH' for x in range(3)]))
-Vjets_zipped = dict(zip(range(4, 15), ['V+jets' for x in range(11)]))
+signal_zipped = dict(zip(range(1, 3), ['VH' for x in range(2)]))
+Vjets_zipped = dict(zip(range(3, 15), ['V+jets' for x in range(12)]))
 ttbar_zipped = {15: 'ttbar'}
 stop_zipped = dict(zip(range(16, 19), ['stop' for x in range(3)]))
 diboson_zipped = dict(zip(range(19, 22), ['diboson' for x in range(3)]))
@@ -68,7 +93,7 @@ df_2jet_filtered = df_2jet.drop(['sample','EventWeight','nJ','nBJ', 'dEtaBB', 'd
 df_3jet_filtered = df_3jet.drop(['sample','EventWeight','nJ','nBJ', 'dEtaBB', 'dPhiBB',
                                  'dEtaWH', 'dPhiLMET', 'BDT', 'pTL', 'etaL', 'Class'], axis=1)
 
-# Convert to ndarray.
+# Convert to ndarray. All elements will be recast as floats.
 dataset_2jet = df_2jet_filtered.as_matrix()
 dataset_3jet = df_3jet_filtered.as_matrix()
 
@@ -102,13 +127,8 @@ test_3jet_process_nos = map(lambda a: event_to_process_nos[a],test_3jet_event_no
 # Map process types numbers to process type groups.
 # Note that this is a convoluted process and could be
 # simplified at a later date...
-test_2jet_processes = np.array(map(lambda a: reduced_sample_map[a], test_2jet_process_nos))
-test_3jet_processes = np.array(map(lambda a: reduced_sample_map[a], test_3jet_process_nos))
-
-#################
-#    INDICES    #
-#################
-
+test_2jet_processes = np.array(map(lambda a: sample_map_invert[a], test_2jet_process_nos))
+test_3jet_processes = np.array(map(lambda a: sample_map_invert[a], test_3jet_process_nos))
 
 
 #################
@@ -130,9 +150,9 @@ twoclass_output = np.array(bdt.decision_function(test_2jet))
 plot_range = (twoclass_output.min(), twoclass_output.max())
 plt.subplot(122)
 
-plot_colors = "rbgym"
+plot_colors = 2*"r" +  12*"g" + "y" + 3*"b" + 3*"m"
 plot_step = 0.02
-class_names = ['VH', 'V+jets', 'ttbar', 'stop', 'diboson']
+class_names = sample_map.keys()
 
 for n, c in zip(class_names, plot_colors):
     plt.hist(twoclass_output[test_2jet_processes == n],
@@ -143,7 +163,7 @@ for n, c in zip(class_names, plot_colors):
              stacked=True)
 x1, x2, y1, y2 = plt.axis()
 plt.axis((x1, x2, y1, y2 * 1.2))
-plt.legend(loc='upper right')
+# plt.legend(loc='upper right')
 plt.ylabel('Samples')
 plt.xlabel('Score')
 plt.title('Decision Scores')
