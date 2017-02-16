3bcddb4d259bc6020e16920875031174d8c09d5b
James Pyne <james.pyne.13@ucl.ac.uk>
2017-02-16 22:16:01 +0000

    Began TrafoD algorithm. Test hist JSON files created.

--- skl_analysis/sensitivity.py
+++ skl_analysis/sensitivity.py
@@ -3,12 +3,48 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-df = pd.read_csv('VHbb_data_truncated.csv')
+data = np.loadtxt('VHbb_data_truncated.csv', delimiter=',')
 
 
-def calculate_sensitivity(signal_counts, background_counts, signal_bins, background_bins):
-    """Routine to calculate sensitivity from a given S/B histogram"""
+def trafoD(signal_dict, background_dict, initial_bins=100):
+    """
+    Routine to calculate perform TrafoD transform on S/B data.
+    Make sure it's PREWEIGHTED.
+    """
 
-    signal_counts.remove
+    # Get signal type name strings.
+    signal_types = signal_dict.keys()
+    background_types = background_dict.keys()
+
+    # Plot range should be between -1 and 1.
+    plot_range = (-1, 1)
+
+    # Get baseline hist counts and bins.
+    signal_hists, signal_bins = plt.hist(signal_dict.values(),
+                                        bins=initial_bins,
+                                        range=plot_range)[:2]
+
+    background_hists, background_bins = plt.hist(background_dict.values(),
+                                                bins=initial_bins,
+                                                range=plot_range)[:2]
 
 
+    signal_hists = hist_count_separate(signal_hists)
+    background_hists = hist_count_separate(background_hists)
+
+    zs = 10
+    zb = 10
+
+    while True:
+        
+
+
+def hist_count_separate(cum_array_of_hists):
+
+    array_of_hists = []
+    while len(array_of_hists) > 1:
+        this_hist = array_of_hists[-1] - array_of_hists[-2]
+        array_of_hists.append(this_hist)
+        array_of_hists.pop()
+
+    return array_of_hists[::-1] # Reverse the array of hists.
