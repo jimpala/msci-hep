c3872cd8bb3e3d60cd623c34179c6c4b7f5933eb
James Pyne <james.pyne.13@ucl.ac.uk>
2017-02-17 15:32:50 +0000

    TrafoD routine completed; beginning debug.

--- skl_analysis/sensitivity.py
+++ skl_analysis/sensitivity.py
@@ -50,7 +50,7 @@ def trafoD(signal_dict, background_dict, initial_bins=200):
     background_freqs = dict(zip(background_types, background_hists))
 
     # Create dict structure for new signal freqs.
-    # Essentially malloc.
+    # Essentially malloc procedure.
     new_signal_freqs = dict(zip(signal_types, [[0] * 100 for _ in range(len(signal_types))]))
     new_background_freqs = dict(zip(background_types, [[0] * 100 for _ in range(len(background_types))]))
 
@@ -58,17 +58,20 @@ def trafoD(signal_dict, background_dict, initial_bins=200):
     zb = 10
     z = 0 # Initialise z.
     bin_i = 1 # Initialise new_bin.
+    interval = 0
+    new_bin_centres = []
 
     for bin_min in bin_mins[::-1]:
+        interval += bin_width
         sig_tot = 0
         back_tot = 0
-        for s, hist in zip(signal_types, signal_hists):
-            this_count = len([a for a in hist if a >= bin_min and a < bin_min + bin_width])
+        for s, data in zip(signal_types, signal_data):
+            this_count = len([a for a in data if a >= bin_min and a < bin_min + bin_width])
             new_signal_freqs[s][-bin_i] += this_count
             sig_tot += this_count
 
-        for b, hist in zip(background_types, background_hists):
-            this_count = len([a for a in hist if a >= bin_min and a < bin_min + bin_width])
+        for b, data in zip(background_types, background_data):
+            this_count = len([a for a in data if a >= bin_min and a < bin_min + bin_width])
             new_background_freqs[b][-bin_i] += this_count
             back_tot += this_count
 
@@ -76,10 +79,22 @@ def trafoD(signal_dict, background_dict, initial_bins=200):
         print z
 
         if z > 1:
+            new_bin_centres.insert(0,z)
+
             z = 0
+            interval = 0
             bin_i +=1
 
-        
+    # Entry for final bin.
+    new_bin_centres.insert(0, z)
+
+    # Filter out the excess memory.
+    for sig in signal_types:
+        new_signal_freqs[sig] = [a for a in new_signal_freqs[sig] if a != 0]
+    for back in background_types:
+        new_background_freqs[back] = [a for a in new_background_freqs[back] if a != 0]
+
+    new_signal_freqs, new_background_freqs, new_bin_centres
 
 
 def hist_count_separate(cum_array_of_hists):
