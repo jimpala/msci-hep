b90327d0aa5266578e96c5d3cc89fa34d6026ba2
James Pyne <james.pyne.13@ucl.ac.uk>
2017-03-01 23:32:30 +0000

    Submission script amended.

--- skl_analysis/ucl_gridsearch_submit.sh
+++ skl_analysis/ucl_gridsearch_submit.sh
@@ -5,10 +5,10 @@ source /home/jpyne/gridsearch-venv/bin/activate
 cd /home/jpyne/msci-hep/skl_analysis
 
 # Iterate through gridsearch args.
-kfolds = 2
+kfolds=2
 for (($kfolds; kfolds<=3; kfolds++))
 do
-	njets = 2
+	njets=2
 	for (($njets; njets<=3; njets++))
 	do
 		python crossValidatorBDT.py $njets $kfolds
