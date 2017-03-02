c71741f0c5c25439e8a5598df0151eed5702b06b
James Pyne <james.pyne.13@ucl.ac.uk>
2017-03-02 18:01:53 +0000

    TrafoD with erros routine alpha built.

--- skl_analysis/sensitivity.py
+++ skl_analysis/sensitivity.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 from event_obj import *
+from copy import copy
 
 
 def trafoD(event_list, initial_bins=1000, z_s=10, z_b=10):
@@ -73,6 +74,91 @@ def trafoD(event_list, initial_bins=1000, z_s=10, z_b=10):
     finally:
         bins.insert(0,-1.0)
         return bins
+
+
+def trafoD_with_error(event_list, initial_bins=1000, z_s=10, z_b=10):
+    """Output optimised histogram bin widths from a list of events"""
+
+    # Check that all event decision values are populated
+    for e in event_list:
+        assert e.decision_value
+
+    # Sort event list by BDT value, descending.
+    event_list = sorted(event_list, key=lambda a: a.decision_value)
+
+    # Get total signal and background.
+    N_tot = sum([a.post_fit_weight for a in event_list])
+    N_s = sum([a.post_fit_weight for a in event_list if a.classification == 1])
+    N_b = N_tot - N_s
+
+    # Set up scan parameters.
+    scan_points = np.linspace(-1, 1, num=initial_bins).tolist()[1:-1]
+    scan_points = scan_points[::-1]
+
+    # Initialise z and bin list.
+    z = 0
+    bins = [1.0]
+    sum_w2_s = 0
+    sum_w2_b = 0
+    delta_bins_s = list()
+    delta_bins_b = list()
+
+    print "TrafoD: Beginning loop."
+
+    try:
+        # Iterate over bin low edges in scan.
+        for p in scan_points:
+            # Initialise freq count for this bin
+            sig_bin = 0
+            back_bin = 0
+
+            # Current bin loop.
+            # Remember, events are in descending DV order.
+            while True:
+                # End algo if no events left - update z and then IndexError.
+                if not event_list:
+                    z += z_s * sig_bin / N_s + z_b * back_bin / N_b
+                    if z > 1:
+                        bins.insert(0, p)
+                    raise IndexError
+
+                # Break if DV not in bin.
+                if event_list[-1].decision_value < p:
+                    break
+
+                # Pop the event.
+                this_event = event_list.pop()
+
+                # Add freq to S/B count, and the square to sums of w2.
+                if this_event.classification == 1:
+                    sig_bin += this_event.post_fit_weight
+                    sum_w2_s += this_event.post_fit_weight ** 2
+                else:
+                    back_bin += this_event.post_fit_weight
+                    sum_w2_b += this_event.post_fit_weight ** 2
+
+            # Update z for current bin.
+            z += z_s * sig_bin / N_s + z_b * back_bin / N_b
+
+            # Reset z and update bin
+            if z > 1:
+                bins.insert(0, p)
+                z = 0
+
+                # Update sum_w2 for this bin.
+                delta_bins_s.insert(0, sum_w2_s)
+                delta_bins_b.insert(0, sum_w2_b)
+                sum_w2_s = 0
+                sum_w2_b = 0
+
+    except IndexError:
+        print "TrafoD: All events processed."
+
+    finally:
+        bins.insert(0,-1.0)
+        delta_bins_s.insert(0, sum_w2_s)
+        delta_bins_b.insert(0, sum_w2_b)
+        return bins, [delta_bins_s, delta_bins_b]
 
 
 def trafoD_tuples(y, y_pred, w, initial_bins=200, z_s=10, z_b=10):
